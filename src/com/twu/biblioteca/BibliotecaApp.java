ba16415fcb846d0e6f27e6d00a22b6c145cbbc05
amandasv <amandasv.pm@gmail.com>
2018-02-26 14:39:16 -0300

    print list book on console

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,9 @@
 package com.twu.biblioteca;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
@@ -7,7 +11,23 @@ public class BibliotecaApp {
         Welcome welcome = new Welcome();
         Printer printer = new Printer();
 
-        printer.print(welcome.message("Hello, Welcome to Bangalore Public Library"));
 
+        printer.print(welcome.message("Hello, Welcome to Bangalore Public Library! \n\n"));
+
+        BookService bookService = new BookService(createBookList());
+
+        printer.print("Here is the list of all books available \n\n");
+        printer.print(bookService.listAll());
+
+
+
+
+    }
+
+    private static List<Book> createBookList(){
+        Book book1 = new Book("Harry Potter");
+        Book book2 = new Book("Head First");
+        List<Book> bookList = Arrays.asList(book1, book2);
+        return bookList;
     }
 }
