c40a3d6161e4761cf6d9856e12698047abfd36c0
amandasv <amandasv.pm@gmail.com>
2018-02-28 18:07:15 -0300

    update menu with option about checkout book

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -2,12 +2,14 @@ package com.twu.biblioteca;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Menu {
 
     private List<Option> optionList;
     private Printer printer = new Printer();
     private BookService bookService = new BookService();
+    Scanner reader = new Scanner(System.in);
 
     public Menu() {
         this.optionList = createMenuList();
@@ -28,15 +30,22 @@ public class Menu {
                 break;
             case 1 :
                 printer.println(optionList.get(1).getTitle() + "\n");
+                bookService.showAvailableList();
 
+                if (bookService.availableList().isEmpty()) {
+                    printer.print("Sorry, but don't have available books");
+                } else {
+                    printer.print("Choose number of book: ");
+                    int index = reader.nextInt();
+                    bookService.checkout(index);
+                }
                 break;
-
         }
     }
 
     private static List<Option> createMenuList(){
-        Option listBooks = new Option("List Book", 0);
-        Option option2 = new Option("Add Book", 1);
+        Option listBooks = new Option("List Books", 0);
+        Option option2 = new Option("Checkout Book", 1);
         Option exit = new Option("Exit", 9);
         List<Option> menuItens = Arrays.asList(listBooks, option2, exit);
         return menuItens;
