10de7760970ea57e6d5bc52001632634af7eef41
amandasv <amandasv.pm@gmail.com>
2018-02-28 13:57:49 -0300

    refactor main

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,35 +1,34 @@
 package com.twu.biblioteca;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
 
-        Welcome welcome = new Welcome();
         Printer printer = new Printer();
         Scanner reader = new Scanner(System.in);
-
-
-        printer.print(welcome.message("Hello, Welcome to Bangalore Public Library! \n"));
+        Menu menu = new Menu();
         int option = 0;
 
-        Menu menu = new Menu(createMenuList());
-
+        printer.print("Hello, Welcome to Bangalore Public Library! \n");
 
         while (option != 9) {
-            printer.print("\n\n----------------------------\n");
+            printer.print("\n----------------------------\n");
             printer.print("-----------Menu-------------\n\n");
 
-            printer.print(menu.showOptions());
+            menu.showOptions();
             printer.print("\n----------------------------\n");
 
             printer.print("Please, choose an option: ");
-            option = reader.nextInt();
+
+            if (reader.hasNextInt()) {
+                option = reader.nextInt();
+            } else {
+                printer.println("Ops, write a valid number!");
+                break;
+            }
+
             menu.chooseOption(option);
 
             if (option == 9) {
@@ -37,14 +36,5 @@ public class BibliotecaApp {
             }
         }
 
-    }
-
-
-    private static List<Option> createMenuList(){
-        Option listBooks = new Option("List Book", 1);
-        Option option2 = new Option("Option 2", 2);
-        Option exit = new Option("Exit", 9);
-        List<Option> menuItens = Arrays.asList(listBooks, option2, exit);
-        return menuItens;
     }
 }
