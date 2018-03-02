5c915279785cc5f0421adc67073f1583a82d4fdb
amandasv <amandasv.pm@gmail.com>
2018-03-02 18:51:27 -0300

    refactor setup tests

--- test/com/twu/biblioteca/BookServiceTest.java
+++ test/com/twu/biblioteca/BookServiceTest.java
@@ -10,9 +10,15 @@ import static org.junit.Assert.assertNotEquals;
 
 public class BookServiceTest {
 
+    private BookService bookService;
+
+    @Before
+    public void setup() {
+         bookService = new BookService();
+    }
+
     @Test
     public void initializesBookServicesWithAListOfBooks() {
-        BookService bookService = new BookService();
         int numbersTotalList = 4;
 
         Assert.assertEquals(numbersTotalList,bookService.listAll().size());
@@ -20,8 +26,6 @@ public class BookServiceTest {
 
     @Test
     public void returnsOnlyAvailableList(){
-        BookService bookService = new BookService();
-
         Book book2 = new Book("Head First Java", "Kathy Sierra, Bert Bates", "2003", true);
         Book book3 = new Book("Happy: Simple Steps for Getting the Life You Want", "Ian K. Smith M.D.", "2010", true);
         Book book4 = new Book("The World I Dream Of", "Curt Butz", "2010", true);
@@ -33,19 +37,15 @@ public class BookServiceTest {
 
     @Test
     public void returnsUnavailableList(){
-        BookService bookService = new BookService();
         Book harryPotter = new Book("Harry Potter", "J. K. Rowling", "1997", false);
 
         List<Book> unavailableList = Arrays.asList(harryPotter);
 
         Assert.assertEquals(bookService.unavailableList(), unavailableList);
-
     }
 
     @Test
     public void returnsFalseIfThatListIsDifferent() {
-        BookService bookService = new BookService();
-
         assertNotEquals(bookService.listAll(), bookService.availableList());
     }
 
